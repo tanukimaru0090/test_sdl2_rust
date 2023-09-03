53cdc229b790d74f1c9deba1b8916e9b3c2c7854
tanukimaru0090 <TANUSAN1015@gmail.com>
2023-09-03 16:56:54 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod sdl2_common;
 mod sdl2_window;
 use sdl2_common::*;
 use sdl2_window::*;
-
+//Main Function
 fn main() -> Result<(), String> {
     let mut sdl_window =Sdl2Window::new();
     sdl_window.create_window("てっすとー", Sdl2Vector2f::new(1280, 800).unwrap()).unwrap();
